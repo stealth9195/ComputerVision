c298b8efb0f024fbeee7f3e9f9092bc99de9aa9a
ShinDongHoon <satosi17@naver.com>
2023-03-20 17:33:06 +0900

    commit_3 hsv

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ src = cv2.imread('c:/Nature99(Small)/picture01.jpg')
 
 ## 영상처리/컴퓨터 비전 알고리즘 적용
 dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
-
+dst2 = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
 # 영상 디스플레이
 cv2.imshow('src', src)
 cv2.imshow('dst1', dst1)
-
+cv2.imshow('dst2', dst2)
 
 # 마무리
 cv2.waitKey(0)
