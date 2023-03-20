accf0e2e62b8ca788fc9ab63e5fba71695561b5c
ShinDongHoon <satosi17@naver.com>
2023-03-20 17:30:48 +0900

    gray scale 변환 기능 추가

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import cv2
 # 영상 소스 열기
 src = cv2.imread('c:/Nature99(Small)/picture01.jpg')
 
+## 영상처리/컴퓨터 비전 알고리즘 적용
+dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
+
 
 # 영상 디스플레이
 cv2.imshow('src', src)
+cv2.imshow('dst1', dst1)
+
 
 # 마무리
 cv2.waitKey(0)
